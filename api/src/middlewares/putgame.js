3adef69325584ae752546faa35753a626553e9ea
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-04-28 10:51:41 -0500

    ruta put modularizada

---
+++ api/src/middlewares/putgame.js
@@ -0,0 +1,14 @@
+const { Videogame, Genre } = require('../db')
+const updategame = async (id, params) => {
+    await Videogame.update(params, {
+        where: { id }
+    })
+
+    const updateVideogame = await Videogame.findOne({
+        where: { id }
+    })
+
+    await updateVideogame.setGenres(genresid)
+}
+
+module.exports = updategame
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const { Router } = require('express');
-
-const { Videogame, Genre } = require('../db')
 const allgames = require('../controllers/getAllvideogames.js')
 const getById = require('../controllers/getById.js')
 const chargegenres = require('../controllers/chargegenres.js');
 const postgame = require('../controllers/postgame');
 const platforms = require('../controllers/platforms.js')
+const putgame = require('../controllers/putgame.js')
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -27,28 +26,6 @@ router.post('/videogame', postgame)
 
 router.get('/platforms', platforms)
 
-router.put('/update/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        let {genresid, ...params} = req.body
-        
-        await Videogame.update(params, {
-            where: {id}
-        })
+router.put('/update/:id', putgame)
 
-        const updateVideogame = await Videogame.findOne({
-            where: {id}
-        })
-
-        await updateVideogame.setGenres(genresid)
-
-        res.status(200).json(await Videogame.findOne({
-            where: {id}
-        }))
-
-    } catch (error) {
-        res.status(404).json(error)
-    }
-
-})
 module.exports = router;
