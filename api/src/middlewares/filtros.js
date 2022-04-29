cc3b02ace2e901f3e67cc42d7467446a97ec4305
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-04-28 20:12:25 -0500

    only api

--- api/src/middlewares/filtros.js
+++
@@ -1,6 +0,0 @@
-function filter(filt, all) {
-    const allVideogames = all
-    const genresfilter = filt === 'ALL' ? allVideogames : allVideogames.filter(el => el.genres.includes(filt))
-    return genresfilter
-}
-module.exports = filter
