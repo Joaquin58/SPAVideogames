6760fb236ad2fc164f477bd0a8f6891db03d40bf
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-04-29 23:58:28 -0500

    prueba de fech y git deploy

--- api/src/middlewares/filtros.js
+++ api/src/middlewares/filtros.js
@@ -1,4 +1,3 @@
-//modificado
 function filter(filt, all) {
     const allVideogames = all
     const genresfilter = filt === 'ALL' ? allVideogames : allVideogames.filter(el => el.genres.includes(filt))
