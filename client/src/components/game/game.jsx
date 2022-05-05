9f637a91c4ad8b9696413178dd8380aaa931fd9d
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-05-04 22:56:05 -0500

    Revert "fix conflict"
    This reverts commit 66c8330a397ffe1a9a958cd805669cde2b4d0ab3.

---
+++ client/src/components/game/game.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams, Link } from 'react-router-dom'
+import { getVideogameById } from '../../redux/actions'
+import imagedefault from '../images/createdefault.jpg'
+import Loading from '../images/charge.gif'
+import gamestyles from './game.module.css'
+
+const Game = () => {
+
+    let { vgid } = useParams();
+    const dispatch = useDispatch()
+    const idvideogame = useSelector(state => state.idvideogame)
+
+    useEffect(() => {
+        dispatch(getVideogameById(vgid))
+        return () => {
+            dispatch(getVideogameById())
+        }
+    }, [dispatch, vgid])
+
+    if (idvideogame) {
+        var {id, image, genres, description, name, platforms, rating, released, CreatedInDb } = idvideogame
+        var defaultimage = image === null || image === undefined ? imagedefault : image.length===0 ? imagedefault : image
+    }
+    
+    return (
+        <>
+            {!idvideogame ? <img src={`${Loading}`} alt='Loanding...' /> :
+                <>
+                    <h1 className={gamestyles.title}>{name}</h1>
+                    {image === undefined ? <img src={`${Loading}`} alt='Loanding...' />
+                        : <div className={gamestyles.contend}>
+                            <div className={gamestyles.top}>
+                                <div className={gamestyles.contentdates}>
+                                    <div className={gamestyles.descriptionconteiner}><div className={gamestyles.description} dangerouslySetInnerHTML={{ __html: description }} /></div>
+                                </div>
+                                <img className={gamestyles.imagevg} src={`${defaultimage}`} alt='notfound' />
+                            </div>
+                            <div className={gamestyles.mapsconteiner}>
+                                <div>
+                                    <h5 className={gamestyles.subtitles}>Rating:</h5>
+                                    <p className={gamestyles.rating}>{rating ? rating : 'notfound'}</p>
+                                    <h5 className={gamestyles.subtitles}>Released:</h5>
+                                    <p className={gamestyles.rating}>{released ? released: 'no disponible'}</p>
+                                </div>
+                                <div> <h5 className={gamestyles.subtitles}>Generos</h5> {
+                                    genres === undefined ? <p>Loading...</p>
+                                        : CreatedInDb ? genres.map(({name}) => {
+                                            return <div className={gamestyles.rating} key={name}>{name}</div>
+                                        })
+                                            : genres.map(gen => {
+                                                return <div className={gamestyles.rating} key={gen}>{gen}</div>
+                                            })}
+                                </div>
+                                <div> <h5 className={gamestyles.subtitles}>Plataformas</h5> {
+                                    platforms?.map(gen => {
+                                        return <div className={gamestyles.rating} key={gen}>{gen}</div>
+                                    })
+                                }</div>
+                            </div>
+                            <Link to='/home'>
+                                <button className={gamestyles.button}>Home</button>
+                            </Link>
+                        </div>
+                    }
+                </>
+            }
+        </>
+    )
+}
+
+export default Game
---
+++ client/src/redux/actions.js
@@ -0,0 +1,168 @@
+import axios from 'axios';
+export const GET_VGS = 'GET_VGS'                //* Todos los juegos
+export const GET_VG = 'GET_VG'                  //* Un solo juego que coincidan con un nombre
+export const GET_VG_BY_ID = 'GET_VG_BY_ID'      //* Un solo juego por id
+export const GET_GN = 'GET_GN'                  //* Todos los generos
+export const POST_VG = 'POST_VG'                //* Postear un juego
+export const FL_DBVG = 'FL_DBVG'                //* Filtro de los juegos creados por el usuario
+export const ORD_BYNM = 'ORD_BYNM'              //* ordenado por nombre
+export const ORD_BYRT = 'ORD_BYRT'              //* ordenado por nombre
+export const GET_PT = 'GET_PT'                  //* obtener plataformas desponibles
+export const ORD_BYNMBK = 'ORD_BYNMBK'
+export const SAVENAME = 'SAVENAME'
+export const FLV_BYGNBK = 'FLV_BYGNBK'
+export const ORDER_TYPE = 'ORDER_TYPE'
+export const PAG_UPDATE = 'PAG_UPDATE'
+export const UPDATE_GAME = 'UPDATE_GAME'
+
+// const URL = 'http://localhost:3001'
+
+
+export const getVideogames = () => {
+    return async (dispatch) => {
+        try {
+            const response = await axios.get(`/videogames`)
+            const payload = response.data
+            return dispatch({ type: GET_VGS, payload })
+        } catch (err) {
+            return dispatch({ type: GET_VGS, payload: ['error'] })
+        }
+    }
+}
+
+export const getVideogameByName = (name) => {
+    return async (dispatch) => {
+        const response = await axios.get(`/videogames?name=${name}`)
+        const payload = response.data
+        dispatch({ type: GET_VG, payload })
+    }
+}
+
+export const getVideogameById = (id) => {
+    return async (dispatch) => {
+        if (id) {
+            try {
+                const response = await axios.get(`/videogames/${id}`)
+                const payload = response.data
+                if (payload) {
+                    dispatch({ type: GET_VG_BY_ID, payload })
+
+                } else {
+                    dispatch({ type: GET_VG_BY_ID, payload: null })
+                }
+
+            } catch {
+                dispatch({ type: GET_VG_BY_ID, payload: null })
+            }
+        } else {
+            dispatch({ type: GET_VG_BY_ID, payload: undefined })
+        }
+
+    }
+}
+
+export const getGenres = () => {
+    return async (dispatch) => {
+        const response = await axios.get(`/genres`)
+        const payload = response.data
+
+        dispatch({ type: GET_GN, payload })
+    }
+}
+
+export const postVideogame = (body) => {
+    return async (dispatch) => {
+        const increate = await axios.post(`/videogame`, body);
+        const payload = await increate.data;
+        dispatch({ type: POST_VG, payload })
+    }
+}
+
+export const filterVideogamesCreated = (payload) => {
+    return {
+        type: FL_DBVG, payload
+    }
+}
+
+export const orderVideogamesByRaiting = (payload) => {
+    return {
+        type: ORD_BYRT, payload
+    }
+}
+
+export const getPlatforms = () => {
+    return async (dispatch) => {
+        const response = await axios.get(`/platforms`)
+        const payload = response.data
+        dispatch({ type: GET_PT, payload })
+    }
+}
+
+export const orderVideogamesByNameBk = (name, order) => {
+    return async (dispatch) => {
+        if (!name) {
+            try {
+                const response = await axios.get(`/videogames?order=${order}`)
+                const payload = response.data
+                return dispatch({ type: ORD_BYNMBK, payload })
+            } catch (err) {
+                return dispatch({ type: ORD_BYNMBK, payload: err })
+            }
+        } else if (name && order) {
+            const response = await axios.get(`/videogames?name=${name}&order=${order}`)
+            const payload = response.data
+            dispatch({ type: ORD_BYNMBK, payload })
+        }
+    }
+}
+
+export const filterVideogamesAndNameBk = (name, order, filtro) => {
+    return async (dispatch) => {
+        if (!(name || order || filtro)) {
+            try {
+                const response = await axios.get(`/videogames`)
+                const payload = response.data
+                return dispatch({ type: FLV_BYGNBK, payload })
+            } catch (err) {
+                return dispatch({ type: FLV_BYGNBK, payload: ['error'] })
+            }
+        } else if (name || order || filtro) {
+            try {
+                const response = await axios.get(`/videogames?name=${name ? name : ''}&order=${order ? order : ''}&filtro=${filtro ? filtro : ''}`)
+                const payload = response.data
+                return dispatch({ type: FLV_BYGNBK, payload })
+            } catch (error) {
+
+            }
+        }
+
+    }
+}
+
+export const orderchange = (payload) => {
+    if (payload === undefined) {
+        return { type: ORDER_TYPE, payload: '' }
+    } else {
+        return { type: ORDER_TYPE, payload }
+    }
+}
+
+export const saveName = (payload) => {
+    return {
+        type: SAVENAME, payload
+    }
+}
+
+export const savePage = (payload) => {
+    return {
+        type: PAG_UPDATE, payload
+    }
+}
+
+export const updateGame = (id) => {
+    return async (dispatch) => {
+        const { data } = await axios.put('/update/:id')
+        const payload = data
+        dispatch({ type: UPDATE_GAME, payload })
+    }
+}
