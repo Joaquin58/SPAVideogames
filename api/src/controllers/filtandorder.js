b57d42beedbeb2176e48b11426191af532eaa54f
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-05-11 22:12:01 -0500

    Agregado el servicio que funciona con todos los filtros

--- api/src/controllers/filtandorder.js
+++ api/src/controllers/filtandorder.js
@@ -1,7 +1,19 @@
-const filtandorder = (req, res) => {
-    const { status, genres, raiting, alfabet} = req.body
+const { filterexist, filterbygenres, orderbyalfabet, filterexistbyname } = require('../middlewares/filterandorder.js')
+const filtandorder = async (req, res) => {
+    const { name, status, genres, alfabet } = req.body
     try {
-        res.status(200).json('conectado')
+        if (name.lenght > 0) {
+            const existorcreate = await filterexist(status)
+            const filtergenres = await filterbygenres(genres, existorcreate)
+            const orderbyabc = await orderbyalfabet(alfabet, filtergenres)
+            return res.status(200).json(orderbyabc)
+        } else {
+            const existorcreate = await filterexistbyname(status, name)
+            if (!existorcreate.length > 0) return res.status(404).json('No Encotrado')
+            const filtergenres = await filterbygenres(genres, existorcreate)
+            const orderbyabc = await orderbyalfabet(alfabet, filtergenres)
+            return res.status(200).json(orderbyabc)
+        }
     } catch (error) {
         return res.status(500).json
     }
