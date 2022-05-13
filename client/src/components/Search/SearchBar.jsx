bd6ce47a6ca7117197549a6ef4daf8c624d94b0f
Joaquin58 <ramirezmendozajoaquinrodrigo@gmail.com>
2022-05-12 20:05:33 -0500

    fix(searchbar.jsx): trabaja junto al estado de búsqueda

--- client/src/components/Search/SearchBar.jsx
+++ client/src/components/Search/SearchBar.jsx
@@ -1,36 +1,40 @@
 import React /*,  { useState  }*/ from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
-    getVideogameByName,
+    // getVideogameByName,
     saveName,
-    savePage
+    // savePage
 } from '../../redux/actions.js'
 import styles from './search.module.css'
 
-const SearchBar = ({setCurrentPage}) => {
+const SearchBar = ({ setCurrentPage, setFilters, filters }) => {
     const dispatch = useDispatch()
     const name = useSelector(state => state.savename)
 
     function handleSearch(e) {
         e.preventDefault()
         dispatch(saveName(e.target.value))
+        setFilters({ ...filters, [e.target.name]: e.target.value })
     }
-    async function handleSubmit(e) {
-        e.preventDefault()
-        await dispatch(getVideogameByName(name))
-        setCurrentPage(1)
-        dispatch(savePage('1'))
-        dispatch(saveName(''))
-    }
+    // async function handleSubmit(e) {
+    //     e.preventDefault()
+    //     await dispatch(getVideogameByName(name))
+    //     setCurrentPage(1)
+    //     dispatch(savePage('1'))
+    //     dispatch(saveName(''))
+    // }
     return (
-        <form className={styles.form} onSubmit={e => handleSubmit(e)}>
+        // <form className={styles.form} onSubmit={searchbyfilters}>
+        <>
             <input className={styles.SearchBar} type='text'
                 placeholder="Navega por los juegos"
                 value={name}
                 onChange={e => handleSearch(e)}
+                name='name'
             />
             <button className={styles.button} type="Submit">Buscar</button>
-        </form>
+        </>
+        // </form>
     )
 }
 
