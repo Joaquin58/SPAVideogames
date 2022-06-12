7c25ff3b90770cd6b5838499a857aff6b412875c
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-06-11 21:41:49 -0500

    add reducer and actions to redux config

--- client/src/redux-toolkit/actions.js
+++ client/src/redux-toolkit/actions.js
@@ -1,11 +1,25 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
-import { GET_VGS, GET_GN, VACIAR_ESTADO } from './consts';
+import {
+    GET_VGS,
+    GET_GN,
+    GET_VG_BY_ID,
+    POST_VG,
+    ORD_BYRT,
+    GET_PT,
+    ORDER_TYPE,
+    SAVENAME,
+    PAG_UPDATE,
+    UPDATE_GAME,
+    DELETE_GAME,
+    ORD_BYNM
+} from './consts';
 import axios from 'axios';
+import { FILT_AND_ORDER } from '../redux/actions';
 
 
 
 
-export const getasyncgames = createAsyncThunk(GET_VGS, async () => {
+export const getVideogames = createAsyncThunk(GET_VGS, async () => {
     try {
         const { data } = await axios.get(`/videogames`)
         const payload = data
@@ -13,17 +27,61 @@ export const getasyncgames = createAsyncThunk(GET_VGS, async () => {
     } catch (err) {
         return ['error']
     }
-}
-)
+})
+
+export const getVideogameById = createAsyncThunk(GET_VG_BY_ID, async (id) => {
+    if (!id) return undefined
+    try {
+        const { data } = await axios.get(`/videogames/${id}`)
+        if (data) return data
+        else return null
+    } catch (error) {
+        return null
+    }
+})
 
 export const getGenres = createAsyncThunk(GET_GN, async () => {
     const { data } = await axios.get(`/genres`)
     const payload = data
-
     return payload
-    // dispatch({ type: GET_GN, payload })
 })
 
-export const vaciarestado = createAction(VACIAR_ESTADO, () => {
-    return []
+// !trello
+export const postVideogame = createAsyncThunk(POST_VG, async (body) => {
+    const { data } = await axios.post(`/videogame`, body);
+    return data
 })
+
+export const orderVideogamesByRaiting = createAction(ORD_BYRT, payload => payload)
+
+export const getPlatforms = createAsyncThunk(GET_PT, async () => {
+    const { data } = await axios.get(`/platfoms`);
+    return data
+})
+
+export const orderchange = createAction(ORDER_TYPE, payload => payload ? "" : payload)
+
+export const saveName = createAction(SAVENAME, payload => payload)
+
+export const savePage = createAction(PAG_UPDATE, payload => payload)
+
+export const updateGame = createAsyncThunk(UPDATE_GAME, async (id, input) => {
+    const { data } = await axios.put(`/update/${id}`, input)
+    return data
+})
+
+export const deletegame = createAsyncThunk(DELETE_GAME, async (id) => {
+    const { data } = await axios.delete(`/delete/${id}`)
+    return data
+})
+
+export const filterandorder = createAsyncThunk(FILT_AND_ORDER, async (filters) => {
+    try {
+        const { data } = await axios.post(`/filtandorder`, filters)
+        return data
+    } catch (error) {
+        return []
+    }
+})
+
+export const orderVideogamesByName = createAction(ORD_BYNM, payload => payload)
--- client/src/redux-toolkit/reducer.js
+++ client/src/redux-toolkit/reducer.js
@@ -1,8 +1,18 @@
 import { createReducer } from "@reduxjs/toolkit";
 import {
-    getasyncgames,
+    getVideogames,
     getGenres,
-    vaciarestado
+    getVideogameById,
+    orderVideogamesByRaiting,
+    postVideogame,
+    getPlatforms,
+    saveName,
+    orderchange,
+    savePage,
+    updateGame,
+    deletegame,
+    filterandorder,
+    orderVideogamesByName,
 } from './actions'
 
 
@@ -22,11 +32,12 @@ export const initialState = {
 
 const rootReducer = createReducer(initialState, builder =>
     builder
-        .addCase(getasyncgames.fulfilled, (state, { payload }) => {
+        .addCase(getVideogames.fulfilled, (state, { payload }) => {
             return {
                 ...state,
                 videogames: payload,
-                allVideogames: payload
+                allVideogames: payload,
+                loading: false
             }
         })
         .addCase(getGenres.fulfilled, (state, { payload }) => {
@@ -35,20 +46,116 @@ const rootReducer = createReducer(initialState, builder =>
                 loading: false,
                 genres: payload
             }
-        }).addCase(getGenres.rejected, (state, { payload }) => {
+        })
+        .addCase(getVideogameById.fulfilled, (state, { payload }) => {
             return {
                 ...state,
-                genres: [],
-                loading: true
+                idvideogame: payload
             }
         })
-        .addCase(vaciarestado, (state, { payload }) => {
+        .addCase(orderVideogamesByRaiting, (state, { payload }) => {
+            const ratingorder = payload === 'max' ? state.videogames.sort(function (a, b) {
+                if (a.rating < b.rating) {
+                    return 1;
+                }
+                if (a.rating > b.rating) {
+                    return -1;
+                }
+
+                return 0;
+            }) : payload === 'min' ? state.videogames.sort(function (a, b) {
+                if (a.rating > b.rating) {
+                    return 1;
+                }
+                if (a.rating < b.rating) {
+                    return -1;
+                }
+
+                return 0;
+            }) : state.videogames
             return {
                 ...state,
-                genres: payload
+                videogames: ratingorder
             }
         })
-        
+        .addCase(postVideogame.fulfilled, (state, { payload }) => {
+            return {
+                ...state
+            }
+        })
+        .addCase(getPlatforms.fulfilled, (state, { payload }) => {
+            return {
+                ...state,
+                plataformas: payload
+            }
+        })
+        .addCase(saveName, (state, { payload }) => {
+            return {
+                ...state,
+                savename: payload
+            }
+        })
+        .addCase(orderchange, (state, { payload }) => {
+            return {
+                ...state,
+                ordertype: payload
+            }
+        })
+        .addCase(savePage, (state, { payload }) => {
+            return {
+                ...state,
+                statePag: payload
+            }
+        })
+        .addCase(updateGame.fulfilled, (state, { payload }) => {
+            return {
+                ...state
+            }
+        })
+        .addCase(deletegame.fulfilled, (state, { payload }) => {
+            return {
+                ...state
+            }
+        })
+        .addCase(filterandorder, (state, { payload }) => {
+            return {
+                ...state,
+                videogames: payload
+            }
+        })
+        .addCase(orderVideogamesByName, (state, { payload }) => {
+            const nameorder = payload === 'asd' ?
+                state.videogames.sort(function (a, b) {
+                    const onename = a.name.toLowerCase()
+                    const twoname = b.name.toLowerCase()
+                    if (onename > twoname) {
+                        return 1;
+                    }
+                    if (onename < twoname) {
+                        return -1;
+                    }
+
+                    return 0;
+                })
+                : payload === 'des' ? state.videogames.sort(function (a, b) {
+                    const onename = a.name.toLowerCase()
+                    const twoname = b.name.toLowerCase()
+                    if (onename < twoname) {
+                        return 1;
+                    }
+                    if (onename > twoname) {
+                        return -1;
+                    }
+                    return 0;
+                }) : state.videogames
+            return {
+                ...state, videogames: nameorder
+            }
+        })
+        .addDefaultCase((state, { payload }) => {
+            return state
+        })
+
 )
 
 export default rootReducer;
