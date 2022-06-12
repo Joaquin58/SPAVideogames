ce074c71f63226e4e5407daecee66bf0ef7e95b4
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-06-11 21:42:34 -0500

    omitidos los casos que no se usan

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 export const GET_VGS = 'GET_VGS'                //* Todos los juegos
-export const GET_VG = 'GET_VG'                  //* Un solo juego que coincidan con un nombre
+// export const GET_VG = 'GET_VG'                  //* Un solo juego que coincidan con un nombre
 export const GET_VG_BY_ID = 'GET_VG_BY_ID'      //* Un solo juego por id
 export const GET_GN = 'GET_GN'                  //* Todos los generos
 export const POST_VG = 'POST_VG'                //* Postear un juego
-export const FL_DBVG = 'FL_DBVG'                //* Filtro de los juegos creados por el usuario
+// export const FL_DBVG = 'FL_DBVG'                //* Filtro de los juegos creados por el usuario
 export const ORD_BYNM = 'ORD_BYNM'              //* ordenado por nombre
 export const ORD_BYRT = 'ORD_BYRT'              //* ordenado por rating
 export const GET_PT = 'GET_PT'                  //* obtener plataformas desponibles
-export const ORD_BYNMBK = 'ORD_BYNMBK'
+// export const ORD_BYNMBK = 'ORD_BYNMBK'
 export const SAVENAME = 'SAVENAME'
-export const FLV_BYGNBK = 'FLV_BYGNBK'
+// export const FLV_BYGNBK = 'FLV_BYGNBK'
 export const ORDER_TYPE = 'ORDER_TYPE'
 export const PAG_UPDATE = 'PAG_UPDATE'
 export const UPDATE_GAME = 'UPDATE_GAME'
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,8 +1,8 @@
 import {
   GET_VGS,
   GET_GN,
-  FL_DBVG,
-  GET_VG,
+  // FL_DBVG,
+  // GET_VG,
   GET_VG_BY_ID,
   ORD_BYRT,
   POST_VG,
@@ -42,18 +42,18 @@ const rootReducer = (state = initialState, { type, payload }) => {
       return {
         ...state, genres: payload
       }
-    case FL_DBVG:
-      const allvideogames = state.allVideogames
-      const cratedfilter = payload === 'Created' ? allvideogames.filter(el => el.CreatedInDb === true)
-        : payload === 'All' ? state.allVideogames
-          : allvideogames.filter(el => el.CreatedInDb === false)
-      return {
-        ...state, videogames: cratedfilter
-      }
-    case GET_VG:
-      return {
-        ...state, videogames: payload
-      }
+    // case FL_DBVG:
+    //   const allvideogames = state.allVideogames
+    //   const cratedfilter = payload === 'Created' ? allvideogames.filter(el => el.CreatedInDb === true)
+    //     : payload === 'All' ? state.allVideogames
+    //       : allvideogames.filter(el => el.CreatedInDb === false)
+    //   return {
+    //     ...state, videogames: cratedfilter
+    //   }
+    // case GET_VG:
+    //   return {
+    //     ...state, videogames: payload
+    //   }
     case GET_VG_BY_ID:
       return {
         ...state, idvideogame: payload
@@ -68,7 +68,7 @@ const rootReducer = (state = initialState, { type, payload }) => {
         }
 
         return 0;
-      }) :  payload === 'min' ? state.videogames.sort(function (a, b) {
+      }) : payload === 'min' ? state.videogames.sort(function (a, b) {
         if (a.rating > b.rating) {
           return 1;
         }
@@ -91,19 +91,19 @@ const rootReducer = (state = initialState, { type, payload }) => {
         ...state,
         plataformas: payload
       }
-    case ORD_BYNMBK:
-      return {
-        ...state, videogames: payload
-      }
+    // case ORD_BYNMBK:
+    //   return {
+    //     ...state, videogames: payload
+    //   }
     case SAVENAME:
       return {
         ...state,
         savename: payload
       }
-    case FLV_BYGNBK:
-      return {
-        ...state, videogames: payload
-      }
+    // case FLV_BYGNBK:
+    //   return {
+    //     ...state, videogames: payload
+    //   }
     case ORDER_TYPE:
       return {
         ...state, ordertype: payload
@@ -148,7 +148,7 @@ const rootReducer = (state = initialState, { type, payload }) => {
             return -1;
           }
           return 0;
-        }): state.videogames
+        }) : state.videogames
       return {
         ...state, videogames: nameorder
       }
