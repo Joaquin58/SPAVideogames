c40eb0ccfc80002fab5abaa530ef81b4daa4efbf
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-06-14 22:43:24 -0500

    fix(redux-toolkit/store.js):configuracion de la store y los reducers

--- client/src/redux-toolkit/store.js
+++ client/src/redux-toolkit/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit"
 import thunk from "redux-thunk"
 // import rootReducer from './reducer'
-import genres from './slices/index'
+import genres from './slices/genres.slice'
+import videogames from "./slices/videogames.slice"
+import platforms from "./slices/platforms.slice"
+import general from "./slices/general.slice"
 
 const store = configureStore({
-    reducer: genres,
-    middleware:[thunk]
+    reducer: {
+        videogames,
+        genres,
+        platforms,
+        general
+    },
+    middleware: [thunk]
 })
 
 export default store
