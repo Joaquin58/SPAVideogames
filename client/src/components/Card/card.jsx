13886414c18ab9cfccae9defde439e0821095786
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-08-01 21:41:35 -0500

    feat: lazy load images

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -4,13 +4,13 @@ import imagedefault from '../images/createdefault.jpg'
 import cardstyles from './card.module.css'
 
 const game = ({ id, image, name, genres }) => {
-    var defaultimage =  image === null || image === undefined || image.length === 0 ? imagedefault : image
+    var defaultimage = image === null || image === undefined || image.length === 0 ? imagedefault : image
     return (
         <div className={cardstyles.caja}>
             <div className={cardstyles.box}>
                 <Link to={`../videogame/${id}`} >
                     <h5 className={cardstyles.tytle}>{name}</h5>
-                    <img className={cardstyles.image} src={`${defaultimage}`} alt='Not found' />
+                    <img className={cardstyles.image} src={`${defaultimage}`} loading = "lazy" alt='Not found' />
                     <div className={cardstyles.genres}>
                         {genres.map(g => {
                             return <div className={cardstyles.contend} key={g}><p>{g}</p></div>
