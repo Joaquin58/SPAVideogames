86b925054949b3671abc489596291030fdb32982
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-08-01 21:45:29 -0500

    feat: card loading

---
+++ client/src/components/cardpage/cardpage.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import React from 'react'
+
+function CardPage() {
+    return (
+        <div style={{
+            backfgroundColor: "#E8EDED",
+            minHeight: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "1rem"
+        }}
+        >
+            CardPage
+        </div>
+    )
+}
+
+export default CardPage
