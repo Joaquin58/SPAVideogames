06e524557f2c9a3ea11197911390a0b3ea75e516
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-08-02 21:49:03 -0500

    feat: definido el modelo de la card

--- client/src/components/cardpage/cardpage.jsx
+++ client/src/components/cardpage/cardpage.jsx
@@ -1,19 +1,36 @@
-import React from "react"
-
-import React from 'react'
-
-function CardPage() {
+import React, { useRef } from "react"
+import "./carpage.css"
+function CardPage({ id, image, name, genres }) {
+    const header = useRef('header')
+    const title = useRef('title')
+    const excerpt = useRef('excerpt')
+    const profile_img = useRef('profile_img')
+    const names = useRef('name')
+    const date = useRef('date')
+    const animated_bgs = useRef('.animated-bg')
     return (
-        <div style={{
-            backfgroundColor: "#E8EDED",
-            minHeight: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "1rem"
-        }}
-        >
-            CardPage
+        <div className="card">
+            <div className="card-header animated-bg" id="header" ref={header}>{name ? name : "&nbsp;"}</div>
+            <div className="card-content">
+                <h3 className="card-title animated-bg animated-bg-text" id="title" ref={title}>
+                    &nbsp;
+                </h3>
+                <p className="card-excerpt" id="excerpt" ref={excerpt}>
+                    &nbsp;
+                    <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
+                    <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
+                    <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
+                </p>
+                <div className="author">
+                    <div className="profile-img animated-bg" id="profile_img" ref={profile_img}>&nbsp;</div>
+                    <div className="author-info">
+                        <strong className="animated-bg animated-bg-text" id="name" ref={names}
+                        >&nbsp;</strong
+                        >
+                        <small className="animated-bg animated-bg-text" id="date" ref={date}>&nbsp;</small>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
