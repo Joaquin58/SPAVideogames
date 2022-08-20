8ceda63389b0b8dee2c5dcaebf19f1ff2d1db77a
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-08-19 22:32:26 -0500

    feat: prerender

--- client/src/components/rendercards/rendercards.jsx
+++ client/src/components/rendercards/rendercards.jsx
@@ -2,21 +2,39 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import CardPage from '../cardpage/cardpage'
-import { getVideogames } from '../../redux-toolkit/actions'
+import { getVideogames, killcomponent } from '../../redux-toolkit/actions'
+import render from "./render.module.css"
+
 const RenderCards = () => {
     const dispatch = useDispatch()
     const allVideogames = useSelector(({ videogames }) => videogames.videogames)
     const Videogames = useSelector(({ videogames }) => videogames.allVideogames)
+    const loader = useSelector(({ videogames }) => videogames.loadinggames)
+
     useEffect(() => {
         dispatch(getVideogames())
+        return dispatch(killcomponent())
     }, [dispatch])
-    return (
-        <> {
-            Array.isArray(Videogames) && Videogames.length > 0 ?
-                Videogames.map((el) => <CardPage key={el.id} name={el.name} image={el.image} genres={el.genres} id={el.id} />)
-                :
-                <>loading...</>
+
+
+    function _prerender() {
+        let prerender = []
+        for (let i = 0; i < 15; i++) {
+            prerender.push(<CardPage key={`${i}`} />)
         }
+        return prerender
+    }
+
+    return (
+        <>
+            <div className={render.flexbox}>
+                {
+                    !loader ?
+                        Videogames.map((el) => <CardPage key={el.id} name={el.name} image={el.image} genres={el.genres} id={el.id} />)
+                        :
+                        <>{_prerender()}</>
+                }
+            </div>
         </>
     )
 }
