05d9b8d3935615f01231524069786b2c72c44ed9
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-08-23 19:19:40 -0500

    feat: change the logic of lazy

--- client/src/components/cardpage/cardpage.jsx
+++ client/src/components/cardpage/cardpage.jsx
@@ -9,10 +9,10 @@ function CardPage({ id, image, name, genres }) {
     const header = useRef('header')
     const title = useRef('title')
     const excerpt = useRef('excerpt')
-    const profile_img = useRef('profile_img')
-    const names = useRef('name')
-    const date = useRef('date')
-    const animated_bgs = useRef('.animated-bg')
+    // const profile_img = useRef('profile_img')
+    // const names = useRef('name')
+    // const date = useRef('date')
+    // const animated_bgs = useRef('.animated-bg')
 
     const loader = useSelector(({ videogames }) => videogames.loadinggames)
     //! controloar la animacion de carga con un loader
@@ -23,23 +23,27 @@ function CardPage({ id, image, name, genres }) {
             <div className="card">
                 <div className={loader ? "card-header animated-bg" : "card-header"} id="header" ref={header}>{!loader ? <img src={`${defaultimage}`} alt="" loading="lazy" /> : "&nbsp;"}</div>
                 <div className="card-content">
-                    {!loader ? <h3 className={loader ? "card-title animated-bg animated-bg-text" : "card-title"} id="title" ref={title}>
-                        {name}
-                    </h3> : <h3 className={loader ? "card-title animated-bg animated-bg-text" : "card-title"} id="title" ref={title}>&nbsp;</h3>}
+                    {
+                        !loader ?
+                            <h3 className="card-title" id="title" ref={title}>
+                                {name}</h3>
+                            :
+                            <h3 className="card-title animated-bg animated-bg-text" id="title" ref={title}>&nbsp;</h3>
+                    }
                     <p className="card-excerpt" id="excerpt" ref={excerpt}>
-                        <>{genres?.map((g) => <span key={g}>{g} </span>) || <span>&nbsp;</span>}</>
-                        <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
-                        <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
-                        <span className="animated-bg animated-bg-text" ref={animated_bgs}>&nbsp;</span>
+                        <span className={loader ? "animated-bg animated-bg-text" : "gentes-title"}>{genres?.map((g) => `${g} `) || <>&nbsp;</>} </span>
+                        {/* <span className={loader ? "animated-bg animated-bg-text" : ""} ref={animated_bgs}>&nbsp;</span>
+                        <span className={loader ? "animated-bg animated-bg-text" : ""} ref={animated_bgs}>&nbsp;</span>
+                        <span className={loader ? "animated-bg animated-bg-text" : ""} ref={animated_bgs}>&nbsp;</span> */}
                     </p>
-                    <div className="author">
-                        <div className="profile-img animated-bg" id="profile_img" ref={profile_img}>&nbsp;</div>
+                    {/* <div className="author">
+                        <div className={loader ? "profile-img animated-bg": ""} id="profile_img" ref={profile_img}>&nbsp;</div>
                         <div className="author-info">
-                            <strong className="animated-bg animated-bg-text" id="name" ref={names}
+                            <strong className={loader ? "animated-bg animated-bg-text" : ""} id="name" ref={names}
                             >&nbsp;</strong>
-                            <small className="animated-bg animated-bg-text" id="date" ref={date}>&nbsp;</small>
+                            <small className={loader ? "animated-bg animated-bg-text" : ""} id="date" ref={date}>&nbsp;</small>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </Link>
