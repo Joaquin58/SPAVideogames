e45164ddb31ac0c3fde5d54cc355abbae4d4d1f9
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2023-01-14 10:08:20 -0600

    hecho el modulo de renderizacion

--- client/src/components/rendercards/rendercards.jsx
+++ client/src/components/rendercards/rendercards.jsx
@@ -1,23 +1,13 @@
 
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import CardPage from '../cardpage/cardpage'
-import { getVideogames, killcomponent } from '../../redux-toolkit/actions'
 import render from "./render.module.css"
 
 
-export default function RenderCards() {
-    const dispatch = useDispatch()
-    const allVideogames = useSelector(({ videogames }) => videogames.videogames)
-    const Videogames = useSelector(({ videogames }) => videogames.allVideogames)
+export default function RenderCards({ current }) {
+
     const loader = useSelector(({ videogames }) => videogames.loadinggames)
-
-    useEffect(() => {
-        dispatch(getVideogames())
-        return dispatch(killcomponent())
-    }, [dispatch])
-
-    const vg = Videogames.slice(0, 15)
 
     function _prerender() {
         let prerender = []
@@ -31,7 +21,7 @@ export default function RenderCards() {
         <div className={render.flexbox}>
             {
                 !loader ?
-                    vg.map((el) => <CardPage key={el.id} name={el.name} image={el.image} genres={el.genres} id={el.id} />)
+                    current.map((el) => <CardPage key={el.id} name={el.name} image={el.image} genres={el.genres} id={el.id} />)
                     : <>{_prerender()}</>
             }
         </div>
