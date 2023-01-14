e6ca6882aa6a91da8ffd043addc8420afcc1d66f
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2023-01-14 10:41:01 -0600

    fix enviroment names

--- api/src/db.js
+++ api/src/db.js
@@ -3,18 +3,18 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT
 } = process.env;
 
 let sequelize =
 process.env.NODE_ENV === "production"
   ? new Sequelize ({
-      database: DB_NAME,
+      database: PGDATABASE,
       dialect: "postgres",
-      host: DB_HOST,
-      port: DB_PORT,
-      username: DB_USER,
-      password: DB_PASSWORD,
+      host: PGHOST,
+      port: PGPORT,
+      username: PGUSER,
+      password: PGPASSWORD,
       pool: {
         max: 3,
         min: 1,
